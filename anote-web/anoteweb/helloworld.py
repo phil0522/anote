8831e5b849477f335e276c980c6dd0d242aaacfd
phil0522 <phonica@gmail.com>
2014-01-07 20:38:16 +0800

    partial commit last time, commit again

--- anote-web/anoteweb/helloworld.py
+++ anote-web/anoteweb/helloworld.py
@@ -1,13 +1,18 @@
 import webapp2
+import sys
+import os
 
+def fix_path():
+    # credit:  Nick Johnson of Google
+    sys.path.append(os.path.join(os.path.dirname(__file__), 'genfiles'))
 
 class MainPage(webapp2.RequestHandler):
 
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write('Hello, World!')
+        self.response.write(sys.path)
 
-
+fix_path()
 application = webapp2.WSGIApplication([
-    ('/', MainPage),
-], debug=True)
+                                       ('/', MainPage),], debug=True)
