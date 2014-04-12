282db40ebeb397871779a176426220a2b072de8b
phil0522 <phonica@gmail.com>
2014-04-11 18:14:42 -0700

    make the file lint free.

--- anote-web/anoteweb/helloworld.py
+++ anote-web/anoteweb/helloworld.py
@@ -1,18 +1,24 @@
-import webapp2
+""" Hello world python. """
+
 import sys
 import os
 
+import webapp2
+
 def fix_path():
-    # credit:  Nick Johnson of Google
-    sys.path.append(os.path.join(os.path.dirname(__file__), 'genfiles'))
+  """Add genfiles directory to python path."""
+  # credit:  Nick Johnson of Google
+  sys.path.append(os.path.join(os.path.dirname(__file__), 'genfiles'))
 
 class MainPage(webapp2.RequestHandler):
+  """Main page handler."""
 
-    def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, World!')
-        self.response.write(sys.path)
+  def get(self):
+    """For both get and post hander"""
+    self.response.headers['Content-Type'] = 'text/plain'
+    self.response.write('Hello, World!')
+    self.response.write(sys.path)
+ 
 
 fix_path()
-application = webapp2.WSGIApplication([
-                                       ('/', MainPage),], debug=True)
+Application = webapp2.WSGIApplication([('/', MainPage),], debug=True)
